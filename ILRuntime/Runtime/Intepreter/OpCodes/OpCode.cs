297ffde07b52e93ef6b8c105c75216fbd8eb6047
luyikk <547386448@qq.com>
2017-01-06 18:10:40 +0800

    添加了AppDomain的文件路径加载方式，简化了加载调用

--- ILRuntime/Runtime/Intepreter/OpCodes/OpCode.cs
+++ ILRuntime/Runtime/Intepreter/OpCodes/OpCode.cs
@@ -7,10 +7,25 @@ using ILRuntime.CLR.Method;
 
 namespace ILRuntime.Runtime.Intepreter.OpCodes
 {
+  
+    /// <summary>
+    /// IL指令
+    /// </summary>
     struct OpCode
     {
+        /// <summary>
+        /// 当前指令
+        /// </summary>
         public OpCodeEnum Code;
+
+        /// <summary>
+        ///  Int32 操作数
+        /// </summary>
         public int TokenInteger;
+
+        /// <summary>
+        /// Int64 操作数
+        /// </summary>
         public long TokenLong;
     }
 }
