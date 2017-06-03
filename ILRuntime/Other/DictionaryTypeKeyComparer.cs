1bec96ca1bd4e946e2b07330e6e8f67fc70e8b85
Greg Fodor <gfodor@gmail.com>
2017-06-03 13:17:30 -0700

    Keep a fast cache of type metadata, and use it for lookup

---
+++ ILRuntime/Other/DictionaryTypeKeyComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Fast comparer of Type objects, assumes there is at most one Type object for a given FullName
+/// </summary>
+public class DictionaryTypeKeyComparer : IEqualityComparer<Type>
+{
+    public bool Equals(Type x, Type y)
+    {
+        return x == y;
+    }
+
+    public int GetHashCode(Type obj)
+    {
+        return obj.FullName.GetHashCode();
+    }
+}
