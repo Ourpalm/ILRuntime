08e4cb6929bdaed323ea23d0d0c99d4a7e3d0c00
Ruofeng Lin <liiir1985@users.noreply.github.com>
2019-01-04 10:28:17 +0800

    resolved #243 #242

--- TestCases/GenericMethodTest.cs
+++ TestCases/GenericMethodTest.cs
@@ -468,10 +468,16 @@ namespace TestCases
         }
         static void TestMethod<T>(Action<GenericClass<T>> enter)
         {
+            GenericClass<T> ins = new GenericClass<T>();
+            enter(ins);
+
         }
         public static void GenericStaticMethodTest10()
         {
-            TestMethod<int>(null);
+            TestMethod<int>((ins) =>
+            {
+                Console.WriteLine(ins.t);
+            });
         }
 
 
