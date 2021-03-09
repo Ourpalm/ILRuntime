b9c5b28d327c0e5faf8e6f3756b9b3c743aff7e8
liiir1985 <liiir1985@hotmail.com>
2021-03-09 12:38:18 +0800

    Fixed the bug with custom attributes not initializing fields properly. Fixed #462

--- ILRuntime/Reflection/ILRuntimeMethodInfo.cs
+++ ILRuntime/Reflection/ILRuntimeMethodInfo.cs
@@ -17,7 +17,7 @@ namespace ILRuntime.Reflection
         Mono.Cecil.MethodDefinition definition;
         ILRuntime.Runtime.Enviorment.AppDomain appdomain;
 
-        object[] customAttributes;
+        Attribute[] customAttributes;
         Type[] attributeTypes;
         public ILRuntimeMethodInfo(ILMethod m)
         {
@@ -34,7 +34,7 @@ namespace ILRuntime.Reflection
 
         void InitializeCustomAttribute()
         {
-            customAttributes = new object[definition.CustomAttributes.Count];
+            customAttributes = new Attribute[definition.CustomAttributes.Count];
             attributeTypes = new Type[customAttributes.Length];
             for (int i = 0; i < definition.CustomAttributes.Count; i++)
             {
@@ -42,7 +42,7 @@ namespace ILRuntime.Reflection
                 var at = appdomain.GetType(attribute.AttributeType, null, null);
                 try
                 {
-                    object ins = attribute.CreateInstance(at, appdomain);
+                    Attribute ins = attribute.CreateInstance(at, appdomain) as Attribute;
 
                     attributeTypes[i] = at.ReflectionType;
                     customAttributes[i] = ins;
