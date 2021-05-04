833b6d40e61d1c1bb247abb7bf1e911e2f059f4b
liiir1985 <liiir1985@hotmail.com>
2021-05-04 17:45:03 +0800

    Fixed inline code size

--- ILRuntime/Runtime/Intepreter/RegisterVM/JITCompiler.cs
+++ ILRuntime/Runtime/Intepreter/RegisterVM/JITCompiler.cs
@@ -986,7 +986,7 @@ namespace ILRuntime.Runtime.Intepreter.RegisterVM
                     if (!ilm.IsDelegateInvoke && !ilm.IsVirtual && !noJIT && !hasExceptionHandler && !ilm.Compiling)
                     {
                         var def = ilm.Definition;
-                        bool codeSizeOK = ilm.IsRegisterBodyReady ? ilm.BodyRegister.Length <= Optimizer.MaximalInlineInstructionCount : def.Body.Instructions.Count <= Optimizer.MaximalInlineInstructionCount;
+                        bool codeSizeOK = ilm.IsRegisterBodyReady ? ilm.BodyRegister.Length <= Optimizer.MaximalInlineInstructionCount / 2 : def.Body.Instructions.Count <= Optimizer.MaximalInlineInstructionCount;
                         if (!def.HasBody || forceInline || codeSizeOK)
                         {
                             canInline = true;
