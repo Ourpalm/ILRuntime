1287b64261042c211eb22281475a850aacabf252
liiir1985 <liiir1985@hotmail.com>
2021-05-04 18:25:46 +0800

    Fixed a null reference exception in inlining method

--- ILRuntime/Runtime/Intepreter/RegisterVM/JITCompiler.cs
+++ ILRuntime/Runtime/Intepreter/RegisterVM/JITCompiler.cs
@@ -986,11 +986,19 @@ namespace ILRuntime.Runtime.Intepreter.RegisterVM
                     if (!ilm.IsDelegateInvoke && !ilm.IsVirtual && !noJIT && !hasExceptionHandler && !ilm.Compiling)
                     {
                         var def = ilm.Definition;
-                        bool codeSizeOK = ilm.IsRegisterBodyReady ? ilm.BodyRegister.Length <= Optimizer.MaximalInlineInstructionCount / 2 : def.Body.Instructions.Count <= Optimizer.MaximalInlineInstructionCount;
-                        if (!def.HasBody || forceInline || codeSizeOK)
+                        if (!def.HasBody || forceInline)
                         {
                             canInline = true;
                             toInline = ilm;
+                        }
+                        else
+                        {
+                            bool codeSizeOK = ilm.IsRegisterBodyReady ? ilm.BodyRegister.Length <= Optimizer.MaximalInlineInstructionCount / 2 : def.Body.Instructions.Count <= Optimizer.MaximalInlineInstructionCount;
+                            if(codeSizeOK)
+                            {
+                                canInline = true;
+                                toInline = ilm;
+                            }
                         }
                     }
                 }
