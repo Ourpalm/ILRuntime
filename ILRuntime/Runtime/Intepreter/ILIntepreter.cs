bc7b1459c3d91688c9e6318cff2c1e74390dab36
Stephen Leong <stcoda@outlook.com>
2024-09-13 19:23:56 +0800

    Add CrossBindingAdaptorType support for LoadFromFieldReference in ILIntepreter.cs

--- ILRuntime/Runtime/Intepreter/ILIntepreter.cs
+++ ILRuntime/Runtime/Intepreter/ILIntepreter.cs
@@ -5553,6 +5553,10 @@ namespace ILRuntime.Runtime.Intepreter
             {
                 ((ILTypeInstance)obj).PushToStack(idx, dst, this, mStack);
             }
+            else if (obj is CrossBindingAdaptorType)
+            {
+                ((CrossBindingAdaptorType)obj).ILInstance.PushToStack(idx, dst, this, mStack);
+            }
             else
             {
                 CLRType t = AppDomain.GetType(obj.GetType()) as CLRType;
