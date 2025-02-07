1be0d7f7e0b38fd157a04cb29205062d9ec40443
liiir1985 <liiir1985@hotmail.com>
2025-02-07 15:15:20 +0800

    Initial commit for hybrid patch branch

---
+++ HotfixAOT/HotfixClass.cs
@@ -0,0 +1,64 @@
+﻿using ILRuntime.Runtime;
+using System;
+
+namespace HotfixAOT
+{
+    [ILRuntimePatch]
+    public class HotfixClass
+    {
+        public int IntField;
+
+#if PATCHED
+        public int IntFieldAdded;
+#endif
+
+        public HotfixClass(int val)
+        {
+#if PATCHED
+            IntField = val + 100;
+            IntFieldAdded = val + 200;
+#else
+            IntField = val;
+#endif
+        }
+        public void Method(int a, string b, out string result)
+        {
+#if PATCHED
+            result = $"{b}{a + 100}";
+#else
+            result = $"{b}{a}";
+#endif
+        }
+
+        public void MethodRef(ref int a, string b, out string result)
+        {
+#if PATCHED
+            a += 100;
+            result = $"{b}{a++}";
+#else            
+            result = $"{b}{a++}";
+#endif
+        }
+        
+        public int Function(int a)
+        {
+#if PATCHED
+            HotfixClass2 cls = new HotfixClass2(a);
+            return cls.IntValue;
+#else
+            return a + 111;
+#endif
+        }
+    }
+
+    public class HotfixClass2
+    {
+        int intVal;
+
+        public int IntValue => intVal;
+        public HotfixClass2(int val)
+        {
+            intVal = val + 12333;
+        }
+    }
+}
