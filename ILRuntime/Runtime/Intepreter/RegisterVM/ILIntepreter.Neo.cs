e7c6112f4f2ed944accbcbe6d85a334b9d12fab8
liiir1985 <liiir1985@users.noreply.github.com>
2026-06-29 19:24:35 +0800

    neo mode box/unbox

--- ILRuntime/Runtime/Intepreter/RegisterVM/ILIntepreter.Neo.cs
+++ ILRuntime/Runtime/Intepreter/RegisterVM/ILIntepreter.Neo.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 
 using ILRuntime.CLR.TypeSystem;
@@ -88,6 +89,14 @@ namespace ILRuntime.Runtime.Intepreter
             {
                 OpCodeR* ip = ptr;
                 bool returned = false;
+                // Shared locals across case blocks. Declared at method scope so
+                // IL2CPP / non-O3 builds reuse the same stack slot for every case.
+                IType t;
+                ILType ilType;
+                ILTypeInstance ins;
+                StackSlotInfo srcSlot, dstSlot;
+                object obj;
+                int sz, refCnt, srcIdx, dstIdx;
                 while (!returned)
                 {
                     try
@@ -105,10 +114,9 @@ namespace ILRuntime.Runtime.Intepreter
                             case OpCodeREnum.Ret:
                                 if (retDst != null && (returnPrimitiveSize > 0 || returnRefCount > 0))
                                 {
-                                    int srcReg = ip->Register1;
-                                    var slot = localInfos[srcReg];
+                                    srcSlot = localInfos[ip->Register1];
                                     if (returnPrimitiveSize > 0)
-                                        Unsafe.CopyBlock(retDst, frameBase + slot.Offset, (uint)returnPrimitiveSize);
+                                        Unsafe.CopyBlock(retDst, frameBase + srcSlot.Offset, (uint)returnPrimitiveSize);
                                     // Reference part: copy via the backing array when we own it
                                     // (Release path uses UncheckedList<object>). In Debug we run on
                                     // List<object> whose internal array isn't accessible on
@@ -117,16 +125,159 @@ namespace ILRuntime.Runtime.Intepreter
                                     if (returnRefCount > 0)
                                     {
 #if DEBUG && !DISABLE_ILRUNTIME_DEBUG
-                                        int srcRefBase = frameRefBase + slot.RefOffset;
+                                        srcIdx = frameRefBase + srcSlot.RefOffset;
                                         for (int i = 0; i < returnRefCount; i++)
-                                            mStack[retRefBase + i] = mStack[srcRefBase + i];
+                                            mStack[retRefBase + i] = mStack[srcIdx + i];
 #else
                                         var arr = mStack.InnerArray;
-                                        Array.Copy(arr, frameRefBase + slot.RefOffset, arr, retRefBase, returnRefCount);
+                                        Array.Copy(arr, frameRefBase + srcSlot.RefOffset, arr, retRefBase, returnRefCount);
 #endif
                                     }
                                 }
                                 returned = true;
+                                break;
+                            case OpCodeREnum.Initobj:
+                                srcSlot = localInfos[ip->Register1];
+                                t = AppDomain.GetType(ip->Operand);
+                                ilType = t as ILType;
+                                if (ilType != null)
+                                {
+                                    refCnt = 0;
+                                    if (ilType.IsEnum)
+                                        sz = AppDomain.GetPrimitiveSize(ilType.FieldTypes[0]);
+                                    else if (ilType.IsPrimitive)
+                                        sz = AppDomain.GetPrimitiveSize(ilType);
+                                    else if (ilType.IsValueType)
+                                    {
+                                        sz = ilType.TotalPrimitiveSize;
+                                        refCnt = ilType.TotalReferenceCount;
+                                    }
+                                    else
+                                    {
+                                        // Reference type initobj → write null index (-1) into the byte slot.
+                                        *(int*)(frameBase + srcSlot.Offset) = -1;
+                                        break;
+                                    }
+                                    if (sz > 0)
+                                        Unsafe.InitBlock(frameBase + srcSlot.Offset, 0, (uint)sz);
+                                    for (int i = 0; i < refCnt; i++)
+                                        mStack[frameRefBase + srcSlot.RefOffset + i] = null;
+                                }
+                                else
+                                {
+                                    // TODO Step 13: CLR value type Initobj (with/without ValueTypeBinder)
+                                    throw new NotImplementedException("CLR value type Initobj: Step 13");
+                                }
+                                break;
+                            case OpCodeREnum.Box:
+                                srcSlot = localInfos[ip->Register2];
+                                dstSlot = localInfos[ip->Register1];
+                                t = AppDomain.GetType(ip->Operand);
+                                ilType = t as ILType;
+                                if (ilType != null)
+                                {
+                                    if (ilType.IsEnum)
+                                    {
+                                        ins = new ILEnumTypeInstance(ilType);
+                                        sz = AppDomain.GetPrimitiveSize(ilType.FieldTypes[0]);
+                                        if (sz > 0)
+                                        {
+                                            ref byte dstP = ref MemoryMarshal.GetReference(ins.Primitives.AsSpan());
+                                            Unsafe.CopyBlock(ref dstP, ref *(frameBase + srcSlot.Offset), (uint)sz);
+                                        }
+                                    }
+                                    else if (ilType.IsPrimitive)
+                                    {
+                                        // Boxing a primitive IL type isn't a regular path
+                                        // (compiler usually boxes CLR primitives), but handle for completeness.
+                                        ins = ilType.Instantiate(false);
+                                        sz = AppDomain.GetPrimitiveSize(ilType);
+                                        if (sz > 0 && ins.Primitives != null)
+                                        {
+                                            ref byte dstP = ref MemoryMarshal.GetReference(ins.Primitives.AsSpan());
+                                            Unsafe.CopyBlock(ref dstP, ref *(frameBase + srcSlot.Offset), (uint)sz);
+                                        }
+                                    }
+                                    else if (ilType.IsValueType)
+                                    {
+                                        ins = ilType.Instantiate(false);
+                                        CopyFrameToIL(frameBase, srcSlot.Offset, srcSlot.RefOffset,
+                                                      ilType.TotalPrimitiveSize, ilType.TotalReferenceCount,
+                                                      mStack, frameRefBase, ins);
+                                    }
+                                    else
+                                    {
+                                        // Boxing a reference type is a no-op: the same instance flows through.
+                                        srcIdx = *(int*)(frameBase + srcSlot.Offset);
+                                        obj = srcIdx >= 0 ? mStack[srcIdx] : null;
+                                        dstIdx = frameRefBase + dstSlot.RefOffset;
+                                        mStack[dstIdx] = obj;
+                                        *(int*)(frameBase + dstSlot.Offset) = obj != null ? dstIdx : -1;
+                                        break;
+                                    }
+                                    ins.Boxed = true;
+                                    dstIdx = frameRefBase + dstSlot.RefOffset;
+                                    mStack[dstIdx] = ins;
+                                    *(int*)(frameBase + dstSlot.Offset) = dstIdx;
+                                }
+                                else
+                                {
+                                    // TODO Step 13: CLR value type Box (with/without ValueTypeBinder)
+                                    throw new NotImplementedException("CLR value type Box: Step 13");
+                                }
+                                break;
+                            case OpCodeREnum.Unbox:
+                            case OpCodeREnum.Unbox_Any:
+                                srcSlot = localInfos[ip->Register2];
+                                dstSlot = localInfos[ip->Register1];
+                                t = AppDomain.GetType(ip->Operand);
+                                srcIdx = *(int*)(frameBase + srcSlot.Offset);
+                                if (srcIdx < 0)
+                                    throw new NullReferenceException();
+                                obj = mStack[srcIdx];
+                                if (obj == null)
+                                    throw new NullReferenceException();
+                                ilType = t as ILType;
+                                if (ilType != null)
+                                {
+                                    ins = obj as ILTypeInstance;
+                                    if (ins == null)
+                                        throw new InvalidCastException();
+                                    if (ilType.IsEnum)
+                                    {
+                                        sz = AppDomain.GetPrimitiveSize(ilType.FieldTypes[0]);
+                                        if (sz > 0)
+                                        {
+                                            ref byte srcP = ref MemoryMarshal.GetReference(ins.Primitives.AsSpan());
+                                            Unsafe.CopyBlock(ref *(frameBase + dstSlot.Offset), ref srcP, (uint)sz);
+                                        }
+                                    }
+                                    else if (ilType.IsPrimitive)
+                                    {
+                                        sz = AppDomain.GetPrimitiveSize(ilType);
+                                        if (sz > 0 && ins.Primitives != null)
+                                        {
+                                            ref byte srcP = ref MemoryMarshal.GetReference(ins.Primitives.AsSpan());
+                                            Unsafe.CopyBlock(ref *(frameBase + dstSlot.Offset), ref srcP, (uint)sz);
+                                        }
+                                    }
+                                    else if (ilType.IsValueType)
+                                    {
+                                        CopyILToFrame(ins,
+                                                      frameBase, dstSlot.Offset, dstSlot.RefOffset,
+                                                      ilType.TotalPrimitiveSize, ilType.TotalReferenceCount,
+                                                      mStack, frameRefBase);
+                                    }
+                                    else
+                                    {
+                                        throw new InvalidCastException();
+                                    }
+                                }
+                                else
+                                {
+                                    // TODO Step 13: CLR value type Unbox (with/without ValueTypeBinder)
+                                    throw new NotImplementedException("CLR value type Unbox: Step 13");
+                                }
                                 break;
                         }
                         ip++;
@@ -188,6 +339,50 @@ namespace ILRuntime.Runtime.Intepreter
 #endif
 #endif
             return frameBase;
+        }
+
+        // Copies frame byte region + frame mStack refs into an ILTypeInstance.
+        // Used by Box (frame → boxed instance).
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static unsafe void CopyFrameToIL(byte* frameBase, int primOffset, int refOffset,
+                                         int primSize, int refCount,
+                                         AutoList mStack, int frameRefBase,
+                                         ILTypeInstance dst)
+        {
+            if (primSize > 0 && dst.Primitives != null)
+            {
+                ref byte dstP = ref MemoryMarshal.GetReference(dst.Primitives.AsSpan());
+                Unsafe.CopyBlock(ref dstP, ref *(frameBase + primOffset), (uint)primSize);
+            }
+            if (refCount > 0)
+            {
+                var dstRefs = dst.ManagedObjects;
+                int srcBase = frameRefBase + refOffset;
+                for (int i = 0; i < refCount; i++)
+                    dstRefs[i] = mStack[srcBase + i];
+            }
+        }
+
+        // Copies ILTypeInstance contents back to the frame byte region + frame mStack refs.
+        // Used by Unbox / Unbox_Any (boxed instance → frame).
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static unsafe void CopyILToFrame(ILTypeInstance src,
+                                         byte* frameBase, int primOffset, int refOffset,
+                                         int primSize, int refCount,
+                                         AutoList mStack, int frameRefBase)
+        {
+            if (primSize > 0 && src.Primitives != null)
+            {
+                ref byte srcP = ref MemoryMarshal.GetReference(src.Primitives.AsSpan());
+                Unsafe.CopyBlock(ref *(frameBase + primOffset), ref srcP, (uint)primSize);
+            }
+            if (refCount > 0)
+            {
+                var srcRefs = src.ManagedObjects;
+                int dstBase = frameRefBase + refOffset;
+                for (int i = 0; i < refCount; i++)
+                    mStack[dstBase + i] = srcRefs[i];
+            }
         }
     }
 }
